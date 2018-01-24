dbab6a2ad21f8df7bdf65073ef56856467ea4cb1
Tyler <twr349@gmail.com>
2018-01-24 19:14:13 +0000

    DB fixed

--- db/schema.rb
+++ db/schema.rb
@@ -31,8 +31,6 @@ ActiveRecord::Schema.define(version: 20180119153027) do
     t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
@@ -40,6 +38,8 @@ ActiveRecord::Schema.define(version: 20180119153027) do
     t.integer "failed_attempts", default: 0, null: false
     t.string "unlock_token"
     t.datetime "locked_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
