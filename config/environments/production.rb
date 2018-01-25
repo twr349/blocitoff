cb70901a9b91ebe6fdd631a2e2a1f772e2570ec9
Tyler <twr349@gmail.com>
2018-01-25 23:12:47 +0000

    mailer fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: 'pure-stream-81125.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'sevendaystodo.herokuapp.com' }
   
   
 end
