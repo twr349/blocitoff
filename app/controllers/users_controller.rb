2bf0edb56809eb1292dba870c9241516443ad7af
Tyler <twr349@gmail.com>
2018-01-26 15:51:18 +0000

    User destroy added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,11 @@ class UsersController < ApplicationController
     @item = Item.new
     @items = current_user.items
   end
+  
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_sign_in
+  end
+
 end
